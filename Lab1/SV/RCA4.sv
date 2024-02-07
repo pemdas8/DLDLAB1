bd6899855649fc02693bdaca5eedc4911c769d04
pemdas8 <William.poduska@gmail.com>
2024-02-07 08:14:22 -0600

    finished RCA4

--- Lab1/SV/RCA4.sv
+++ Lab1/SV/RCA4.sv
@@ -1,6 +1,9 @@
-module RCA4 (input  logic[3:0] a, b, input logic cin, output logic cout, sum);
+module RCA4 (input  logic[3:0] a, b, input logic cin, output logic [4:0] sum);
    output logic cout[1], cout[2], cout[3];
    
+  silly(a[0], b[0], cin, s[0], cout[1]);
+  silly(a[1], b[1], cout[1], s[1], cout[2]);
+  silly(a[2], b[2], cout[2], s[2], cout[3]);
+  silly(a[3], b[3], cout[3], s[3], s[4]);
   
-   
 endmodule
